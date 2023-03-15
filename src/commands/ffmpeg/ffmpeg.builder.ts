336aa3b00cb71c8b9eb0029ca72f0817cf32f6c9
Mike <spormuv@gmail.com>
2023-03-15 13:40:26 +0300

    feat: add builder for ffmpeg

---
+++ src/commands/ffmpeg/ffmpeg.builder.ts
@@ -0,0 +1,33 @@
+export class FfmpegBuilder {
+  private inputPath: string;
+  private options: Map<string, string> = new Map();
+
+  constructor() {
+    this.options.set('-c:v', 'libx264');
+  }
+
+  input(inputPath: string): this {
+    this.inputPath = inputPath;
+    return this;
+  }
+
+  setVideoSize(width: number, height: number): this {
+    this.options.set('-s', `${width}x${height}`);
+    return this;
+  }
+
+  output(outputPath: string): string[] {
+    if (!this.inputPath) {
+      throw new Error('inputPath is required');
+    }
+    const args: string[] = ['-i', this.inputPath];
+    this.options.forEach((k, v) => {
+      args.push(v);
+      args.push(k);
+    });
+    args.push(outputPath);
+    return args;
+  }
+}
+
+new FfmpegBuilder().input('').setVideoSize(1920, 1080).output('//');
